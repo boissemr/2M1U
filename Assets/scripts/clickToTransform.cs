f21bbd43dc4b5e7075506bd71d5d97bea2a8c568
Matt Boisseau <boissemr@miamioh.edu>
2015-10-14 02:51:51 -0400

    trying to get characterController to work
    character rotation and picking up objects is broken, but the controller
    is otherwise implemented
    
    also, added more control for clickToTransform

--- Assets/scripts/clickToTransform.cs
+++ Assets/scripts/clickToTransform.cs
@@ -2,10 +2,23 @@
 using System.Collections;
 
 public class clickToTransform : MonoBehaviour {
+	
+	[Tooltip("the object that will replace this one when it is clicked on")]			public GameObject	target;
+	[Tooltip("whether starting position will be relative to the companion object")]		public bool			positionRelative;
+	[Tooltip("whether starting rotation will be inherited from the companion object")]	public bool			inheritRotation;
+	[Tooltip("starting position")]														public Vector3		startingPosition;
+	[Tooltip("starting rotation")]														public Quaternion	startingRotation;
 
-	public GameObject target;
+	void Start() {
 
+		// set position and rotation according to whether or not they are set to be relative
+		transform.position = startingPosition + (positionRelative ? transform.position : Vector3.zero);
+		if(!inheritRotation) transform.rotation = startingRotation;
+	}
+	
 	void OnMouseDown() {
+
+		// create the target in this object's place
 		Instantiate(target, this.transform.position, this.transform.rotation);
 		Object.Destroy(this.gameObject);
 	}
--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -15,16 +15,26 @@ public class playerController : MonoBehaviour {
 	GameObject		pickup,
 					carrying;
 	LayerMask		pickupable;
+	CharacterController controller;
+	Vector3			movement;
 
 	// start
 	void Start() {
+
+		// initialize variables
+		movement = Vector3.zero;
+		controller = GetComponent<CharacterController>();
+
+		// layer masks
 		pickupable = 1 << 8;
 	}
 
 	// update
 	void Update() {
+
 		// detect all pickupable objects around the player
-		pickups = Physics.SphereCastAll(transform.position, pickupReach*.66f, transform.forward, pickupReach, pickupable);
+		//pickups = Physics.SphereCastAll(transform.position, pickupReach*.66f, transform.forward, pickupReach, pickupable);
+		pickups = Physics.SphereCastAll(transform.position, pickupReach, transform.forward, 0, pickupable);
 
 		// pick one (the closest) out of the pickupable objects
 		if(pickups.Length > 0) {
@@ -34,43 +44,51 @@ public class playerController : MonoBehaviour {
 		}
 
 		// pick up or drop stuff
-		if(Input.GetButtonDown("Fire1")) {
+		if(Input.GetButtonDown("Grab")) {
 			if(carrying == null && pickups.Length > 0) {
 				// pick it up
 				carrying = pickup;
-				transform.LookAt(new Vector3(carrying.transform.position.x, transform.position.y, carrying.transform.position.z));
+				//transform.LookAt(new Vector3(carrying.transform.position.x, transform.position.y, carrying.transform.position.z));
 				carrying.transform.position = transform.position + (Quaternion.Euler(0, transform.eulerAngles.y, 0) * carryingPosition);
 			} else {
 				// drop it
 				carrying = null;
 			}
 		}
-
-		// jump
-		if(Input.GetButtonDown("Jump") && Grounded()) {
-			GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpForce, 0));
+		if(carrying != null) {
+			// drop it if it gets snagged
+			if(Vector3.Distance(transform.position, carrying.transform.position) > pickupReach) {
+				carrying = null;
+			}
 		}
 	}
 
 	// fixedupdate
 	void FixedUpdate() {
-		// get player inputs
-		float h = Input.GetAxis("Horizontal");
-		float v = Input.GetAxis("Vertical");
 
-		// move player around
-		Vector3 movement = new Vector3(h, 0, v)*((carrying != null)?carryingSpeed:walkSpeed);
-		transform.position += movement;
+		// character controller
+		Quaternion r = transform.rotation;
+		if(controller.isGrounded) {
+			movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			movement = transform.TransformDirection(movement);
+			movement *= walkSpeed;
+			if(Input.GetButton("Jump")) movement.y = jumpForce;
+		}
+		movement.y -= 20 * Time.deltaTime;
+		transform.rotation = Quaternion.identity;
+		controller.Move(movement * Time.deltaTime);
+		transform.rotation = r;
 
 		// carrying and player rotation
 		if(carrying != null) {
 			// if we are carrying something, we move it and look at it
 			carrying.transform.position += movement;
-			transform.LookAt(new Vector3(carrying.transform.position.x, transform.position.y, carrying.transform.position.z));
+			//transform.LookAt(new Vector3(carrying.transform.position.x, transform.position.y, carrying.transform.position.z));
 		} else if(movement.magnitude > .05) {
 			// if we're not carrying anything, we look ahead
-			transform.LookAt(transform.position + movement);
+			//transform.LookAt(movement);
 		}
+
 	}
 	
 	// sort two objects by distance from the player
